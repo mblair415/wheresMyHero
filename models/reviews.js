733d0c3f698c05691b1f56913e6fb45b49bc7f1c
johnson-rl <johnson.rl@gmail.com>
2016-12-21 13:44:42 -0800

    Review model created

--- models/reviews.js
+++ models/reviews.js
@@ -1,6 +1,13 @@
-Review {
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var ReviewSchema = new Schema ({
   stars: Number,
   text: String,
   upvotes: Number,
   recommend: Boolean
-}
+})
+
+
+var Review = mongoose.model('Review', ReviewSchema)
+module.exports = Review
