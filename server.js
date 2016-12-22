dd6349007c7cc737c59cd789017d3d122ef74f17
mblair415 <mblair415@gmail.com>
2016-12-22 13:47:16 -0800

    get all works.  get one may work.  create doesn't seem to work.

--- server.js
+++ server.js
@@ -19,30 +19,63 @@ app.get('/', function(req, res){
 ////////////////////
 ////Routes
 ////////////////////
-var reviewSample = {
+var reviewSample = [{
   stars: 4,
   reviewContent: 'superduper gud',
   recommend: false,
   upvotes: 417
-}
+  },
+  {
+    stars: 1,
+    reviewContent: 'the stuff of nightmares, but on a plate',
+    recommend: true,
+    upvotes: 7
+  }
+]
 
-////Get all reviews
+////Get all reviews -- line 43 should work for seeded data.
 app.get('/api/reviews', function(req, res){
   db.Review.find(function(err, review){
     if(err){
       console.log('Error in server.js', err);
     }
-    console.log('review is ', review);
-    res.send(review);
+    console.log('all reviews are ', review);
+    res.send(review)
+    // res.send(reviewSample);
   });
 });
 
-////Get one review
-
+//Get one review  this may work for seeded data
+app.get('api/reviews/:id', function(req, res) {
+  db.Review.findOne(function(err, review){
+    if(err){
+      console.log('FindOne error in server.js', err);
+    }
+    console.log('your single review is ', review);
+    res.send(review);
+  })
+})
 
 
 ////Create one review
-
+app.post('/api/reviews', function (req, res) {
+  // create new book with form data (`req.body`)
+  var newReview = new db.Review({
+    stars: req.body.stars,
+    reviewContent: req.body.reviewContent,
+    recommend: req.body.recommend,
+    upvotes: req.body.upvotes
+  });
+    // save newBook to database
+    newReview.save(function(err, review){
+      if (err) {
+        return console.log("save review error: " + err);
+      }
+      console.log("saved ", review.reviewContent);
+      // send back the review!
+      res.json(review);
+    });
+});
 
 
 ////Listen
