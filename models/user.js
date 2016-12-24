c3fc3587da46104259d28960fb89ae69667dc682
johnson-rl <johnson.rl@gmail.com>
2016-12-24 13:46:50 -0800

    two new schema created. seed.js data added.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,15 @@
-User {
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var UserSchema = new Schema ({
   firstName: String,
   lastName: String,
   username: String,
   password: String,
   sandwiches: [],
   reviews: []
-}
+});
+
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
