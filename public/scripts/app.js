0cf2ae2c5cdd31e52239b013df94e66555c3cf9c
mblair415 <mblair415@gmail.com>
2016-12-26 15:34:12 -0800

    have gifs loading into individual divs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,9 @@ var giphyApi = "http://api.giphy.com/v1/gifs/search";
 $(document).ready(function(){
   console.log('The DOM body is ready')
 
+  var source = $('#selectableGif-template2').html();
+
+  var template = Handlebars.compile(source);
 /*
 event listener listening for submitition
 prevent default
@@ -50,31 +53,63 @@ server.js tells server and db to handle the call.
       sucess: newGifSearchSuccess,
       error: newGifSearchError
     })
-
-
   })
 
+  // $(window).scroll(function() {
+  //   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+  //   console.log('dude, you scrolled to the bottom');
+  //
+  //    $.ajax({
+  //      method: 'GET',
+  //      url: 'http://api.giphy.com/v1/gifs/search?q=gif-input&api_key=dc6zaTOxFJmzC',
+  //      data: $('form').serialize()+'&offset=25',
+  //      success: giphySearchMoreSuccess,
+  //      error: newGifSearchError
+  //    })
+  //   }
+  // });
 
 
+
+  function newGifSearchSuccess(json){
+    console.log('ajax call for gif successful.  Gif: ', json);
+    $('.deleteThisClass').empty();
+    json.data.forEach(function(gif){
+      var giphyHtml = template({ insertGifHere: gif.images.fixed_width_small.url})
+      $(".gifSelectionField2").append(giphyHtml);
+    });
+
+    // json.data.forEach(function(gif){
+    //   $('.deleteThisClass').append('<img src=' +
+    //   gif.images.fixed_height_small.url + '>')
+    // })
+    // $('.searched-gifs').append(gif.data.forEach)
+  }
 })
 
-function newReviewSuccess(review){
-  console.log('ajax call on review successful.  Review: ', review);
+// function newReviewSuccess(review){
+//   console.log('ajax call on review successful.  Review: ', review);
+// }
+//
+// function newReviewError(error){
+//   console.log('ajax call on review dun messed up.  Error: ', error);
+// }
+
+
+// });
+
+
+function putGifInBox(gif){
+
 }
 
-function newReviewError(error){
-  console.log('ajax call on review dun messed up.  Error: ', error);
-}
-
-function newGifSearchSuccess(gif){
-  console.log('ajax call for gif successful.  Gif: ', gif);
-  $('.deleteThisClass').empty();
-  gif.data.forEach(function(gif){
-    $('.deleteThisClass').append('<img src=' +
-    gif.images.fixed_height_small.url + '>')
-  })
-  // $('.searched-gifs').append(gif.data.forEach)
-}
+// function giphySearchMoreSuccess(json){
+//   console.log('ajax call for MOAR gifs worked.  Gif: ', json);
+//   json.data.forEach(function(gif){
+//     $('.deleteThisClass').append('<img src=' +
+//     gif.images.fixed_height_small.url + '>')
+//   })
+// }
 
 function newGifSearchError(error){
   console.log('ajax call on gif search went bad, boss.  Error: ', error);
