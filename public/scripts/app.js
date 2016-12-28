c9ae8a8fb9b05149c781a739759c68c7eef66ce9
mblair415 <mblair415@gmail.com>
2016-12-28 09:46:34 -0800

    gifs appear separately, gifs can be clicked on, we can track the selection of a gif

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,6 @@
 console.log('sanity check, app.js is connected')
 
-// var giphyApi = "http://api.giphy.com/v1/gifs/search";
+var giphyApi = "http://api.giphy.com/v1/gifs/search";
 
 $(document).ready(function(){
   console.log('The DOM body is ready')
@@ -23,32 +23,37 @@ $(document).ready(function(){
     })
   })
 
-  $('#gifSearchButton').on('submit', function(event){
-    console.log('second gif submission button clicked');
-    event.preventDefault();
-
-    $.ajax({
-      method: 'GET',
-      url: 'http://api.giphy.com/v1/gifs/search?q=gif-input&api_key=dc6zaTOxFJmzC',
-      data: $(this).serializeArray(),
-      sucess: newGifSearchSuccess,
-      error: newGifSearchError
-    })
+  $('.gifSelectionField2').on('click', '.gifBox', function(event){
+    console.log('an image was clicked!', this.src);
   })
 
-  $(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-    console.log('dude, you scrolled to the bottom');
 
-     $.ajax({
-       method: 'GET',
-       url: 'http://api.giphy.com/v1/gifs/search?q=gif-input&api_key=dc6zaTOxFJmzC',
-       data: $('form').serializeArray()+'&offset=25',
-       success: giphySearchMoreSuccess,
-       error: newGifSearchError
-     })
-    }
-  });
+  // $('#gifSearchButton').on('submit', function(event){
+  //   console.log('second gif submission button clicked');
+  //   event.preventDefault();
+  //
+  //   $.ajax({
+  //     method: 'GET',
+  //     url: 'http://api.giphy.com/v1/gifs/search?q=gif-input&api_key=dc6zaTOxFJmzC',
+  //     data: $(this).serializeArray(),
+  //     sucess: newGifSearchSuccess,
+  //     error: newGifSearchError
+  //   })
+  // })
+
+  // $(window).scroll(function() {
+  //   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+  //   console.log('dude, you scrolled to the bottom');
+  //
+  //    $.ajax({
+  //      method: 'GET',
+  //      url: giphyApi,
+  //      data: $('#gifId').serializeArray()+'&offset=25',
+  //      success: giphySearchMoreSuccess,
+  //      error: newGifSearchError
+  //    })
+  //   }
+  // });
 
   function newGifSearchSuccess(json){
     console.log('ajax call for gif successful.  Gif: ', json);
@@ -59,13 +64,13 @@ $(document).ready(function(){
     });
   }
 
-  function giphySearchMoreSuccess(json){
-    console.log('ajax call for MOAR gifs worked.  Gif: ', json);
-    json.data.forEach(function(gif){
-      var giphyHtml = template({ insertGifHere: gif.images.fixed_width_small.url})
-      $(".gifSelectionField2").append(giphyHtml);
-    })
-  }
+  // function giphySearchMoreSuccess(json){
+  //   console.log('ajax call for MOAR gifs worked.  Gif: ', json);
+  //   json.data.forEach(function(gif){
+  //     var giphyHtml = template({ insertGifHere: gif.images.fixed_width_small.url})
+  //     $(".gifSelectionField2").append(giphyHtml);
+  //   })
+  // }
 
 })
 
