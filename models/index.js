48405c7b770066ed9cefb9187befb76691e852e5
johnson-rl <johnson.rl@gmail.com>
2017-01-02 22:48:36 -0800

    rewrote schema and models...now referencing

--- models/index.js
+++ models/index.js
@@ -4,3 +4,4 @@ mongoose.connect('mongodb://localhost/wheres-my-hero');
 module.exports.Review = require('./review.js');
 module.exports.Sandwich = require('./sandwich.js');
 module.exports.User = require('./user.js');
+module.exports.Restaurant = require('./restaurant.js');
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,6 +1,23 @@
-Restaurant {
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var RestaurantSchema = new Schema ({
   name: String,
   location: String,
-  sandwiches: [],
-  reviews: []
-}
+  sandwiches: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Sandwich'
+  }],
+  reviews: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Review'
+  }],
+  restaurants: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Restaurant'
+  }]
+})
+
+
+var Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+module.exports = Restaurant;
